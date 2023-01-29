ce9a69b64395957f0399cca54c19190700326478
Roman Ivanov <roman_ivanov85@internet.ru>
2023-01-29 18:16:19 +0300

    change MainActivity

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
@@ -14,35 +15,15 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val viewModel by viewModels<PostViewModel>()
-        viewModel.data.observe(this) { post ->
-            with(binding)
-            {
-                author.text = post.author
-                published.text = post.published
-                content.text = post.content
-
-                heart.setImageResource(
-                    if (post.likedByMe) R.drawable.red_heart else R.drawable.baseline_favorite_border_24
-                )
-                heartTextView.text = clickCount(post.likeCount)
-                eyesTextView.text = clickCount(post.lookCount)
-                shareTextView.text = clickCount(post.shareCount)
-            }
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostAdapter(
+            { viewModel.heart(it.id) },
+            { viewModel.share(it.id) }
+        )
+        binding.list.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
-        binding.heart.setOnClickListener {
-            viewModel.heart()
-        }
-
-        binding.share.setOnClickListener {
-            viewModel.share()
-        }
-
-        binding.eyes.setOnClickListener {
-            viewModel.eyes()
-        }
-        val textView: TextView = findViewById(R.id.content)
-        textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
 
--- app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -1,6 +1,10 @@
 package ru.netology.nmedia.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
@@ -8,18 +12,29 @@ import ru.netology.nmedia.dto.Post
 typealias OnLikeListener = (post: Post) -> Unit
 typealias OnShareListener = (post: Post) -> Unit
 
-class PostAdapter (
+class PostAdapter(
     private val onLikeListener: OnLikeListener,
-    private val onShareListener: OnShareListener) :
-                    ListAdapter<Post, PostViewHolder>(Post)
+    private val onShareListener: OnShareListener
+) :
+    ListAdapter<Post, PostAdapter.PostViewHolder>(PostDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding, onLikeListener, onShareListener)
+    }
 
-}
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val share = getItem(position)
+        holder.bind(share)
+        val heart = getItem(position)
+        holder.bind(heart)
+    }
 
-class PostViewHolder {
-    private val binding: CardPostBinding,
-    private val onLikeListener: OnLikeListener,
-    private val onShareListener: onShareListener): RecyclerView.ViewHolder(binding.root)
-    {
+
+    class PostViewHolder(
+        private val binding: CardPostBinding,
+        private val onLikeListener: OnLikeListener,
+        private val onShareListener: OnShareListener
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(post: Post) {
             binding.apply {
                 author.text = post.author
@@ -38,15 +53,15 @@ class PostViewHolder {
         }
     }
 
+    class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
+        override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+            return oldItem.id == newItem.id
+        }
 
-}
-
-class PostDiffCallback : DiffUtil.ItemCallback<Post>() {
-    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
-        return oldItem == newItem
+        override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+            return oldItem == newItem
+        }
     }
 }
+
+
