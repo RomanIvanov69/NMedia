2ca6da7a4c28580792a51a2abc57a4f79fda5ff2
Roman Ivanov <roman_ivanov85@internet.ru>
2023-02-04 16:59:48 +0300

    change activity

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,10 +3,13 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.view.View
 import android.widget.TextView
 import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.OnInteractionListener
 import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +19,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostAdapter(
-            { viewModel.heart(it.id) },
-            { viewModel.share(it.id) }
+        val adapter = PostAdapter(object : OnInteractionListener {
+            override fun onShare(post: Post) {
+                viewModel.share(post.id)
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onEyes(post: Post) {
+                viewModel.eyes(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                binding.editGroup.visibility = View.VISIBLE
+                viewModel.edit(post.id)
+            }
+
+            override fun onCancelEdit(post: Post) {
+                viewModel.notEdit()
+            }
+
+        }
         )
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
