217d28bc71593496d9609d80b628b8a04c5f415c
Roman Ivanov <roman_ivanov85@internet.ru>
2023-02-04 16:59:28 +0300

    change repository

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -5,7 +5,10 @@ import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
     fun getAll(): LiveData<List<Post>>
-    fun heart(id: Long)
     fun share(id: Long)
+    fun heart(id: Long)
     fun eyes(id: Long)
+    fun save(post: Post)
+    fun removeById(id: Long)
+    fun likeById(id: Long)
 }
